e5f1c9a7c9b1982dbe37b2d6ed06026256a65221
Jordan <jcr.salazar@outlook.com>
2020-11-19 12:56:12 -0500

    Commit v1

---
+++ src/Player.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Player.css';
+import Sidebar from './Sidebar';
+import Body from './Body';
+
+function Player({ spotify }){
+
+    return(
+        <div className="player">
+            
+            <div className="player__body">
+                <Sidebar />
+                <Body />
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Player;
